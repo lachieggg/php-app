3969e8430a847687aa01cea8f645d7e686fc22c9
Lachlan Grant <grant.lachlan.j@gmail.com>
2021-09-11 11:55:33 +1000

    Made contact endpoint redirect to home after mailto

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -24,6 +24,7 @@ class ContactController extends Controller
 
   public function email($request, $response)
   {
+    header("Location: /");
     header("Location: mailto:lachie@lachiegrant.io");
     exit();
   }
