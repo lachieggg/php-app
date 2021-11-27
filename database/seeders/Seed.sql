c194218c1f4ea70dec098c068d9455e9ffa58576
Lachlan Grant <grant.lachlan.j@gmail.com>
2021-11-27 22:36:11 +1100

    Added seed file and modified sample .env

---
+++ database/seeders/Seed.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS website;
+USE website;
+CREATE TABLE IF NOT EXISTS users (
+    uuid varchar(36) NOT NULL,
+    first_name varchar(255),
+    last_name varchar(255),
+    full_name varchar(255),
+    email varchar(255),
+    password varchar(255),
+    created_at timestamp,
+    updated_at timestamp
+);
+ALTER TABLE website.users ADD is_deleted BOOL NULL;
+ALTER TABLE website.users ADD is_approved BOOL NULL;
+CREATE TABLE IF NOT EXISTS website.user_comments (
+    uuid varchar(36) NULL,
+    user_uuid varchar(36) NULL,
+    comment_text TEXT NULL,
+              created_at DATETIME,
+              updated_at DATETIME
+);
+CREATE TABLE IF NOT EXISTS website.blog_posts (
+    uuid varchar(36) NULL,
+    post_html TEXT NULL,
+              is_private TINYINT,
+              created_at DATETIME,
+              updated_at DATETIME
+);
+ALTER TABLE website.users ADD is_admin BOOL NULL;
+
