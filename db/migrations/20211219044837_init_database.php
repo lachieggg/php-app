ffc22f9f2cd2cf3b38452b28692bdaa688b4b556
Lachlan Grant <grant.lachlan.j@gmail.com>
2021-12-21 20:36:01 +1100

    Added first phinx migration for initial database schema

--- db/migrations/20211219044837_init_database.php
+++ db/migrations/20211219044837_init_database.php
@@ -1,23 +1,67 @@
 <?php
-declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
 final class InitDatabase extends AbstractMigration
 {
     /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
+     * Initialize the database
      */
     public function change(): void
     {
+        // Users
+        $users = $this->table('users');
+        $users->addColumn('uuid', 'string')
+            ->addColumn('email', 'string')
+            ->addColumn('name', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('is_deleted', 'boolean', ['default' => false])
+            ->addColumn('is_approved', 'boolean', ['default' => false])
+            ->addColumn('is_admin', 'boolean', ['default' => false])
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->create();
 
+        $users->addIndex('uuid', array('unique' => true))->save();
+        $users->addIndex('email', array('unique' => true))->save();
+
+
+        // Blog Posts
+        $posts = $this->table('blog_posts');
+        $posts->addColumn('uuid', 'string')
+            ->addColumn('title', 'string')
+            ->addColumn('content', 'string')
+            ->addColumn('type', 'string')
+            ->addColumn('is_private', 'boolean', ['default' => false])
+            ->addColumn('is_deleted', 'boolean', ['default' => false])
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->create();
+
+        $posts->addIndex('uuid', array('unique' => true))->save();
+
+        // User Comments
+        $comments = $this->table('user_comments');
+        $comments->addColumn('uuid', 'string')
+            ->addColumn('user_uuid', 'string')
+            ->addColumn('comment_text', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->create();
+
+        $comments->addIndex('uuid', array('unique' => true))->save();
+
+        // Gallery Posts
+        $gallery = $this->table('gallery_posts');
+        $gallery->addColumn('uuid', 'string')
+            ->addColumn('title', 'string')
+            ->addColumn('image_path', 'string')
+            ->addColumn('description', 'string')
+            ->addColumn('is_deleted', 'boolean', ['default' => false])
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->create();
+
+        $gallery->addIndex('uuid', array('unique' => true))->save();
     }
 }
--- phinx.php
+++ phinx.php
@@ -17,12 +17,12 @@ return
         'default_migration_table' => 'phinxlog',
         'default_environment' => 'development',
         'production' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'production_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
+            'adapter' => $_ENV['DATABASE_DRIVER'],
+            'host' => $_ENV['MYSQL_HOST'],
+            'name' => $_ENV['MYSQL_DATABASE'],
+            'user' => $_ENV['MYSQL_USER'],
+            'pass' => $_ENV['MYSQL_PASSWORD'],
+            'port' => $_ENV['MYSQL_PORT'],
             'charset' => 'utf8',
         ],
         'development' => [
@@ -31,18 +31,9 @@ return
             'name' => $_ENV['MYSQL_DATABASE'],
             'user' => $_ENV['MYSQL_USER'],
             'pass' => $_ENV['MYSQL_PASSWORD'],
-            'port' => '3306',
+            'port' => $_ENV['MYSQL_PORT'],
             'charset' => 'utf8',
         ],
-        'testing' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'testing_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
-        ]
     ],
     'version_order' => 'creation'
 ];
