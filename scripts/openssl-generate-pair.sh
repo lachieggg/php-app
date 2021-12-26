bdaf67be5f2ce32e5ea32499131c5a7fc43fb124
Lachlan Grant <grant.lachlan.j@gmail.com>
2021-12-26 18:48:57 +1100

    Moved SSL scripts to separate repo

--- scripts/openssl-generate-pair.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Generate an OpenSSL RSA Keypair and copy it to the public directory
-
-openssl genrsa -out private.pem 4096
-openssl rsa -in private.pem -outform PEM -pubout -out public.pem
-cp public.pem ../public/ssl
