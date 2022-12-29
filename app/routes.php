3a97004e0d2ca87b6f2d5772d63573d5fd20172d
Lachlan Grant <grant.lachlan.j@gmail.com>
2022-12-29 20:37:08 +1100

    Cleanup

--- app/routes.php
+++ app/routes.php
@@ -9,15 +9,15 @@ $app->get('/home', 'HomeController:index')->setName('home');
 $app->get('/github', 'HomeController:github')->setName('home.github');
 $app->get('/people', 'HomeController:people')->setName('home.people');
 $app->get('/resume', 'HomeController:resume')->setName('home.resume');
-$app->get('/consulting', 'HomeController:consulting')->setName('home.consulting');
 $app->get('/blog', 'HomeController:blog')->setname('home.blog');
 $app->get('/contact', 'ContactController:contact')->setName('contact');
+$app->get('/consulting', 'HomeController:consulting')->setName('home.consulting');
 $app->get('/pgp/public', 'HomeController:publickey')->setName('home.pgp.public');
 
 // Auth
 $app->post('/sign-up', 'AuthController:postSignUp')->setName('auth.sign-up');
-$app->get('/sign-up', 'AuthController:getSignUp')->setName('auth.sign-up');
 $app->post('/sign-in', 'AuthController:postSignIn')->setName('auth.sign-in');
+$app->get('/sign-up', 'AuthController:getSignUp')->setName('auth.sign-up');
 $app->get('/sign-in', 'AuthController:getSignIn')->setName('auth.sign-in');
 $app->get('/email-exists', 'AuthController:getEmailExists')->setName('auth.email-exists');
 $app->get('/change-password', 'AuthController:changePassword')->setName('auth.password.change');
