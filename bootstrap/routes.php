02a9d56ffea251a083b4fc0959e1284669021d3a
Lachlan Grant <grant.lachlan.j@gmail.com>
2023-04-02 15:42:45 +1000

    Remove cv link

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -12,7 +12,6 @@ function defineRoutes($router)
     $router->map(['GET'], '/', 'HomeController:index')->setName('home.root');
     $router->map(['GET'], '/home', 'HomeController:index')->setName('home');
     $router->map(['GET'], '/github', 'HomeController:github')->setName('home.github');
-    $router->map(['GET'], '/resume', 'HomeController:resume')->setName('home.resume');
     $router->map(['GET'], '/blog', 'HomeController:blog')->setName('home.blog');
     $router->map(['GET'], '/pgp/public', 'HomeController:publickey')->setName('home.pgp.public');
 }
